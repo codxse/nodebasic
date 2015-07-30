6a74fde49b5e4a8b708d115e27e651f816f804cc
Nadiar AS <nadiar429@gmail.com>
2015-07-30 07:33:11 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+var start = function(route, handle) {
+	http.createServer(function(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log('Request for ' + pathname + ' recived.');
+
+		route(handle, pathname);
+
+		response.writeHead({
+			"Content-Type": "text/plain"
+		});
+		response.write('Hello World');
+		response.end();
+	}).listen(8888);
+	console.log('Server listen at port 8888');
+};
+
+exports.start = start;
+
