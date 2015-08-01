fb43ff299d8d82d3a609d18c470a86519aabdd4e
Nadiar AS <nadiar429@gmail.com>
2015-08-01 11:46:20 +0700

    ugly index view through requesthandler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,25 @@
-var exec = require('child_process').exec;
-
 var start = function(response) {
 	console.log('Request handler \'start\' was called.');
 
-	exec('ls -lah', function(error, stdout, stderr) {
-		response.writeHead(200, {
-			"Content-Type": "text/plain"
-		});
-		response.write(stdout);
-		response.end();
+	var body = '<doctype html>' +
+		'<html>' +
+		'<head>' +
+		'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+		'</head>' +
+		'<body>' +
+		'<form action="/upload" method="post">' +
+		'<textarea name="text" rows="10" cols="30"></textarea>' + 
+		'<br /><br />' +
+		'<input type="submit" value="Submit text" />' +
+		'</form>' +
+		'</body>' +
+		'</html>';
+
+	response.writeHead(200, {
+		"Content-Type": "text/html"
 	});
+	response.write(body);
+	response.end();		
 };
 
 var upload = function(response) {
